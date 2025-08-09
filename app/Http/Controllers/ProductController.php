9749a1ac1ab6c1c069bfb09d514beb9fb7d27b37
septianmalela <maulanas430@gmail.com>
2025-08-09 20:29:39 +0700

    products filter by product_category_id & name

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,20 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with('product_category')->get();
+        $query = Product::with('product_category');
+
+        if ($request->has('product_category_id')) {
+            $query->where('product_category_id', $request->input('product_category_id'));
+        }
+
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        $products = $query->get();
+
         return response()->json([
             'status' => 'success',
             'products' => $products,
